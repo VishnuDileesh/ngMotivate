ce3c7552e8c280ab4be322c3920f044971b530c7
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-01 17:12:30 +0530

    Add - pass in bg image to component (ngstyles

--- src/app/showimage/showimage.component.ts
+++ src/app/showimage/showimage.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-showimage',
   templateUrl: './showimage.component.html',
-  styleUrls: ['./showimage.component.css']
+  styleUrls: ['./showimage.component.css'],
 })
 export class ShowimageComponent implements OnInit {
+  bgImageUrl: string;
+  bgStyle: any;
 
-  constructor() { }
+  constructor() {
+    this.bgImageUrl =
+      'https://images.unsplash.com/photo-1534067783941-51c9c23ecefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80';
 
-  ngOnInit(): void {
+    this.bgStyle = {
+      backgroundImage: `url(${this.bgImageUrl})`,
+    };
   }
 
+  ngOnInit(): void {}
 }
