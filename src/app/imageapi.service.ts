4d2caa6bd0e5a5c0d0a94043fd6f8a90396af35b
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-02 15:48:14 +0530

    Add - bg image on refresh

--- src/app/imageapi.service.ts
+++ src/app/imageapi.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImageapiService {
-  width = window.innerWidth;
-  height = window.innerHeight;
+  private APIKEY = environment.imageAPIKey;
+  private BASEURL = 'https://api.pexels.com/v1/search?query=nature';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: this.APIKEY,
+    }),
+  };
 
   constructor(private http: HttpClient) {}
 
   fetchBgImage(): Observable<any> {
-    return this.http.get(
-      `https://loremflickr.com/json/g/${this.width}/${this.height}/nature`
-    );
+    return this.http.get(`${this.BASEURL}`, this.httpOptions);
   }
 }
--- src/app/showimage/showimage.component.ts
+++ src/app/showimage/showimage.component.ts
@@ -9,16 +9,38 @@ import { ImageapiService } from 'src/app/imageapi.service';
 export class ShowimageComponent implements OnInit {
   bgImageUrl: string;
   bgStyle: any;
+  photos = [];
+  photo: any;
 
   constructor(private imageapi: ImageapiService) {
     // this.bgImageUrl = `https://picsum.photos/${this.width}/${this.height}.jpg`;
 
-    this.imageapi.fetchBgImage().subscribe((data) => console.log(data));
+    // this.imageapi
+    //   .fetchBgImage()
+    //   .subscribe((data) => console.log(data.photos[0].src.portrait));
 
+    this.getBgImage();
+
+    // this.bgStyle = {
+    //   backgroundImage: `url(${this.bgImageUrl})`,
+    // };
+  }
+
+  ngOnInit(): void {}
+
+  getBgImage(): void {
+    this.imageapi.fetchBgImage().subscribe((data) => {
+      this.photos = data.photos; // data.photos[0].src.portrait;
+      this.photo = this.photos[Math.floor(Math.random() * this.photos.length)];
+      this.bgImageUrl = this.photo.src.portrait;
+
+      this.setBgImage();
+    });
+  }
+
+  setBgImage(): void {
     this.bgStyle = {
       backgroundImage: `url(${this.bgImageUrl})`,
     };
   }
-
-  ngOnInit(): void {}
 }
